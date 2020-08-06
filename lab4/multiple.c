62b65b402f7234ea405d03893b51fc050b0bd43b
Your Name <you@example.com>
2020-08-07 01:55:02 +0600

    lab4 adding

---
+++ lab4/multiple.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+    int a;
+
+    printf("input a number:\a");
+    scanf("%d",&a);
+
+    if(a%7==0){
+        printf("The number is  multiple to 7");
+        }
+
+        else{
+            printf("The number is not multiple to 7");
+        }
+
+         return 0;
+
+}
